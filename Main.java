4acbaec5519a2d276af26fd3e84017b79f64c91a
ajaniSterling <ajani.sterling@hgs.hiddengeniusproject.org>
2021-12-11 02:26:07 +0000

    Developed 2.3 The Math Class

---
+++ Main.java
@@ -0,0 +1,27 @@
+/**
+ *   This program demonstrate
+ *   Class Math methods.
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // To hold data.
+      double x, y, result;
+
+      x = 16;
+      y = 2.0;
+
+      // Get the power.
+      result = Math.pow(x, y);
+
+      // Display result.
+      System.out.println(result);
+
+      // Get square root.
+      result = Math.sqrt(x);
+
+      // Display result.
+      System.out.println(result);
+   }
+}
